83d81929af59bf855ba0caf6e43ad95761fcb332
Denis Zhelev <denis862008@mail.ru>
2017-03-06 08:39:13 +0300

    Тестовое задание для MOS.RU
    - Добавил изменения в верстке
    - Подключил Select2

--- js/dist/main.js
+++ js/dist/main.js
@@ -16692,25 +16692,103 @@ var vendors = __webpack_require__(1);
     vendors();
 })();
 
-
 window.addEventListener('load', function() {
-    var cities = [];
+    var cities = [],
+        staticCities = [
+            {
+                id: 1,
+                text: 'Архангельск'
+            },
+            {
+                id: 2,
+                text: 'Астрахань'
+            },
+            {
+                id: 3,
+                text: 'Санкт-Петербург'
+            },
+            {
+                id: 4,
+                text: 'Сочи'
+            },
+            {
+                id: 5,
+                text: 'Москва'
+            },
+            {
+                id: 6,
+                text: 'Владивосток'
+            },
+            {
+                id: 7,
+                text: 'Новосибирск'
+            },
+            {
+                id: 8,
+                text: 'Нижний Новгород'
+            },
+            {
+                id: 9,
+                text: 'Екатеринбург'
+            },
+            {
+                id: 10,
+                text: 'Краснодар'
+            },
+            {
+                id: 11,
+                text: 'Ростов-на-Дону'
+            },
+            {
+                id: 12,
+                text: 'Белгород'
+            },
+            {
+                id: 13,
+                text: 'Омск'
+            },
+            {
+                id: 14,
+                text: 'Хабаровск'
+            },
+            {
+                id: 15,
+                text: 'Воронеж'
+            }
+        ];
 
-    $.ajax({
-        type: 'GET',
-        url: 'json/cities.json',
-        dataType: 'json',
-        success: function(data) {
-            console.log(data);
-        }
-    });
+    if(!window.location.host) {
+        $('select').select2({
+            data: staticCities,
+            minimumResultsForSearch: Infinity,
+            containerCssClass: 'custom-select',
+            dropdownCssClass: 'custom-select_dropdown'
+        });
+    } else  {
+        $.ajax({
+            type: 'GET',
+            url: 'json/cities.json',
+            dataType: 'json',
+            cache: false,
+            success: function(data) {
+                var items = data.cities;
 
-    $('select').select2({
-        minimumResultsForSearch: Infinity,
-        containerCssClass: 'custom-select',
-        tags: "true",
-        dropdownCssClass: 'custom-select_dropdown'
-    });
+                items.forEach(function(item, index) {
+                    cities.push({
+                        id: index + 1,
+                        text: item.title
+                    });
+                });
+
+                $('select').select2({
+                    data: cities,
+                    minimumResultsForSearch: Infinity,
+                    containerCssClass: 'custom-select',
+                    dropdownCssClass: 'custom-select_dropdown'
+                });
+            }
+        });
+    }
 });
 
 
